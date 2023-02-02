8f7d304db8fe23f34af7a019a01b7a404c0a6068
akhilsuryam <akhilsuryam12@gmail.com>
2023-02-02 17:57:17 +0530

    d

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import data from './data.js';
+// import data from '../data.js';
+
+
+const app = express();
+
+
+app.get('/api/products',(req,res )=>{
+    res.send(data.products)
+})
+
+const port  =process.env.PORT||5000;
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  }).on('error', (err) => {
+    console.error(`An error occurred while starting the server: ${err}`);
+  });
