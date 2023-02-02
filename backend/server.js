089c4df213eacc1a39854f6b2c588d60f74436ca
divyavinod6 <thisisdivyavinod@gmail.com>
2023-02-02 23:17:46 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,20 @@ import express from 'express';
 import data from './data.js';
 // import data from '../data.js';
 
-
 const app = express();
+// import cors from 'cors';
+// var cors = require('cors');
+// app.use(cors());
 
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
-app.get('/api/products',(req,res )=>{
-    res.send(data.products)
-})
-
-const port  =process.env.PORT||5000;
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+app
+  .listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
-  }).on('error', (err) => {
+  })
+  .on('error', (err) => {
     console.error(`An error occurred while starting the server: ${err}`);
   });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+// import data from '../data';
+import axios from 'axios';
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/products');
+      const result = await response.json();
+      setProducts(result);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
